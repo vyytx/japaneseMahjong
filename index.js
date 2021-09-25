4105027f9aaac296e56c190b1399c7920ad36d82
vyytx <dobeender@gmail.com>
2021-09-25 21:49:37 +0800

    send -> sendFile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(__dirname + 'index.html');
+    res.sendFile(__dirname + 'index.html');
 });
 
 app.post("/newGame", (req, res) => {
