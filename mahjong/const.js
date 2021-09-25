4b849df339fff75069f2229e9fa53fad2683561d
vyytx <dobeender@gmail.com>
2021-09-25 23:03:13 +0800

    const and tiles

---
+++ mahjong/const.js
@@ -0,0 +1,13 @@
+export const yiaoJio = [0, 8, 9, 17, 18, 26];
+
+export const East = 27, South = 28, West = 29, North = 30;
+
+export const hakuHatsuChun = [31, 32, 33];
+export const Winds = [East, South, West, North];
+
+export const YiTiles = Winds.concat([hakuHatsuChun]);
+
+export const redFiveMan = 54, redFiveTsutsu = 63, redFiveJo = 72;
+export const redDoras = [redFiveMan, redFiveTsutsu, redFiveJo];
+
+export const starterDoras = redDoras
